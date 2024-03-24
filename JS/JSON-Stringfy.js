3924a3d40c4116a16c2947356ec8c30bd69584e2
Your Namesifathero <sifathero466@gmail.com>
2024-03-24 15:55:31 +0600

    API

---
+++ JS/JSON-Stringfy.js
@@ -0,0 +1,25 @@
+const user ={id:1,name:'ami borokuks', profession: 'actor'}
+const stringDefine =JSON.stringify(user);
+console.log(user);
+console.log(stringDefine);
+/**
+ * output: { id: 1, name: 'ami borokuks', profession: 'actor' }
+{"id":1,"name":"ami borokuks","profession":"actor"}
+ */
+
+// more
+const shop ={
+    name: 'dilara',
+    Address: {
+        Rode: 'Baily Road',
+        Country: 'bd',
+        Sector:'seven',
+        property: 'single'
+    },
+    product:['laptop','mic','mouse','pc','keyboard'],
+    isOpen : true,
+    isNew: false
+}
+console.log(shop);
+const shopJon =JSON.stringify(shop);
+console.log(shopJon);
